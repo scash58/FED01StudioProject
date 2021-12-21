d7651db692674e44466a76ecb6d1511020d58b00
Scott Cashion <scashion58@outlook.com>
2021-12-20 18:32:10 -0600

    Removed all lorem ipsum and added a auto updating  copyright tag in the footer

--- TestProject/js/jquerycode.js
+++ TestProject/js/jquerycode.js
@@ -27,3 +27,27 @@ $(function () {
     });
 
 });
+
+//Create a new Date object to display our copyright info
+let currentDate = new Date(); //Empty constructor -- for Date objects, this defaults to today's date
+
+console.log(currentDate + " -- Current Date");
+
+//Create a variable to take ONLY the year from our currentDate object
+let currentYear = currentDate.getFullYear();
+
+//Remember that JS is an Object Oriented language. This means that objects have properties (props)  that
+//are like nouns, as well as methods which are like verbs.
+//Above, currentYear was created from the Date() consturctor method, which means it can call methods
+//int he Date class. The getFullYear() we used is an example of an instance method.
+
+console.log(currentYear + " -- Current Year");
+
+//Use the value of currentYear as the content for the <span id="copyright">
+//Create a variable to capture the <span> from the HTML document
+let copyright = document.getElementById("copyright");
+
+console.log(copyright);
+
+//To update the content of an element, we can use .textContent
+copyright.textContent = currentYear;
